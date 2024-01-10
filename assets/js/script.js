b0e76aa6a1c9f49be2328661f5f0ba6b3affc0cb
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-01-10 13:42:32 +0600

    update hero-section and add about content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-// Fetch the members data from the JSON file
 const jsonFile = "assets/documents/members.json";
+const membersInfo = document.querySelector("#membersInfo");
+
 fetch(jsonFile)
   .then((response) => {
     if (!response.ok) {
@@ -8,28 +9,56 @@ fetch(jsonFile)
     return response.json();
   })
   .then((members) => {
-    // Access each member's name and display them in the HTML
-    const membersInfo = document.querySelector("#membersInfo");
-    // Loop through the array of members
-    members.forEach((user) => {
-      // HTML Contents or Elements
+    const getAllMembers = document.querySelector("#getAllMembers");
+    // Get All User/Member Index
+    const membersLength = members.length; // 13 members
+    // Display only the first 8 members initially
+    let displayIndex = 8;
+    let limitedMembers = members.slice(0, displayIndex);
+
+    getAllMembers.addEventListener("click", () => {
+      // Update limitedMembers to include all members
+      displayIndex = membersLength;
+      limitedMembers = members;
+
+      // Clear existing content before adding all members
+      membersInfo.innerHTML = "";
+      getAllMembers.style.display = "none";
+      // Loop through the array of limited members (now all members)
+      limitedMembers.forEach((user) => {
+        const userHtml = `
+          <div class="col-xl-3 col-md-4 col-sm-6">
+            <div class="card">
+              <input type="hidden" id="${user.user_id}">
+              <div class="img">
+                <img src="assets/img/${user.image}" class="card-img-top" alt="${user.name}" />
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">${user.name}</h5>
+                <p class="card-text">${user.position}</p>
+                <button type="button" onclick="openMemberModal(${user.user_id})" class="btn btn-color">More Info</button>
+              </div>
+            </div>
+          </div>`;
+        membersInfo.innerHTML += ` ${userHtml} `;
+      });
+    });
+
+    // Loop through the array of limited members initially
+    limitedMembers.forEach((user) => {
       const userHtml = `
         <div class="col-xl-3 col-md-4 col-sm-6">
-        <div class="card">
+          <div class="card">
             <input type="hidden" id="${user.user_id}">
             <div class="img">
-                <img
-                src="assets/img/${user.image}"
-                class="card-img-top"
-                alt="${user.name}"
-                />
+              <img src="assets/img/${user.image}" class="card-img-top" alt="${user.name}" />
             </div>
             <div class="card-body">
-            <h5 class="card-title">${user.name}</h5>
-            <p class="card-text">${user.position}</p>
-            <button type="button" onclick="openMemberModal(${user.user_id})" class="btn btn-color">More Info</button>
+              <h5 class="card-title">${user.name}</h5>
+              <p class="card-text">${user.position}</p>
+              <button type="button" onclick="openMemberModal(${user.user_id})" class="btn btn-color">More Info</button>
             </div>
-        </div>
+          </div>
         </div>`;
       membersInfo.innerHTML += ` ${userHtml} `;
     });
@@ -38,19 +67,7 @@ fetch(jsonFile)
     console.error("Error fetching the members data:", error);
   });
 
-// Function to fetch user data from JSON file
-async function fetchUserData() {
-  try {
-    const response = await fetch(jsonFile);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const userData = await response.json();
-    return userData;
-  } catch (error) {
-    console.error("Error fetching user data:", error);
-  }
-} // End
+// End
 
 // Function to fetch member data from JSON file
 async function fetchMemberData() {
@@ -91,9 +108,12 @@ async function openMemberModal(user_id) {
   const memberLiving = document.querySelector(".memberLiving");
   const socialInfo = document.querySelector("#socialInfo");
 
-  const phone = `<a href="tel:${user.phone}"><i class="bi bi-telephone-outbound-fill"></i></a>`;
-  const wp = `<a target="_blank" href="http://wa.me/${user.phone}"><i class="bi bi-whatsapp"></i></a>`;
-  const fb = `<a target="_blank" href="${user.fb_url}"><i class="bi bi-facebook"></i></a>`;
+  const phone = `
+       <a href="tel:${user.phone}"><i class="bi bi-telephone-outbound-fill"></i></a>`;
+  const wp = `
+       <a target="_blank" href="http://wa.me/${user.phone}"><i    class="bi bi-whatsapp"></i></a>`;
+  const fb = `
+      <a target="_blank" href="${user.fb_url}"><i class="bi bi-facebook"></i></a>`;
 
   memberImg.src = `assets/img/${user.image}`;
   memberName.innerHTML = ` <span class="border-bottom border-info text-color">${user.name}</span>`;
